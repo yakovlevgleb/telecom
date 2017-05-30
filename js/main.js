3eeb45556643964b5a47b826764e3fcd597615bd
Yakovlev Gleb <brin@ya.ru>
2017-05-30 18:06:41 +0500

    fixes #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+"use strict";
+
+var navi = 0;
+
+jQuery(document).ready(function() {
+
+  var backgrounds;
+  var body = jQuery('body');
+  if (body.hasClass('news')) {
+    backgrounds = [
+      'url(./img/news__bg_1.jpg)',
+      'url(./img/news__bg_2.jpg)',
+      'url(./img/news__bg_3.jpg)'
+    ];
+
+    var selectItem = function(index) {
+      navi = index;
+      body.css('background-image', backgrounds[index]);
+    };
+
+    setInterval(function() {
+      if (navi < backgrounds.length) {
+        selectItem(navi);
+        navi = navi + 1;
+      } else {
+        navi = 0;
+        selectItem(navi);
+        navi = navi + 1;
+      }
+    }, 5000);
+
+  }
+});
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav button');
 
