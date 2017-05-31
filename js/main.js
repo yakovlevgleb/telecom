5a3c857b489f5bec5f9dacc91be765a3b44f7147
Yakovlev Gleb <brin@ya.ru>
2017-05-31 14:46:48 +0500

    js fixes

--- js/main.js
+++ js/main.js
@@ -1,36 +1,17 @@
-"use strict";
-
-var navi = 0;
-
 jQuery(document).ready(function() {
+  var images = ['./img/news__bg_1.jpg', './img/news__bg_2.jpg', './img/news__bg_3.jpg'];
+  var i = 0;
 
-  var backgrounds;
-  var body = jQuery('body');
-  if (body.hasClass('news')) {
-    backgrounds = [
-      'url(./img/news__bg_1.jpg)',
-      'url(./img/news__bg_2.jpg)',
-      'url(./img/news__bg_3.jpg)'
-    ];
-
-    var selectItem = function(index) {
-      navi = index;
-      body.css('background-image', backgrounds[index]);
-    };
-
-    setInterval(function() {
-      if (navi < backgrounds.length) {
-        selectItem(navi);
-        navi = navi + 1;
-      } else {
-        navi = 0;
-        selectItem(navi);
-        navi = navi + 1;
+  setInterval(function() {
+    $('.news').animate({},function() {
+      if (i >= images.length) {
+        i = 0;
       }
-    }, 5000);
-
-  }
+      $(this).css('background-image', 'url(' + images[i++] + ')').animate({});
+    });
+  }, 5000);
 });
+
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav button');
